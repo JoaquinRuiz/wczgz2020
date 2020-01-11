dc1421a5a05d18ca51e739688365672d98e3e08a
Joaquin Ruiz <joaquin.ruiz@flat101.es>
2020-01-11 17:48:36 +0100

    added slug prev/next to posts

--- wp-content/plugins/cp-wordcamps/cp-wordcamps.php
+++ wp-content/plugins/cp-wordcamps/cp-wordcamps.php
@@ -11,37 +11,55 @@ License: GPLv2 or later
 Text Domain: jokiruiz
 */
 
-add_action( 'init', 'wordcamps_cpt' );
-function wordcamps_cpt() {
-    $args = array(
-      'public'       => true,
-      'show_in_rest' => true,
-      'label'        => 'Wordcamps'
-    );
-    register_post_type( 'wordcamp', $args );
+// WordCamp Custom Post
+add_action('init', 'wordcamps_cpt');
+function wordcamps_cpt()
+{
+  $args = array(
+    'public'       => true,
+    'show_in_rest' => true,
+    'label'        => 'Wordcamps'
+  );
+  register_post_type('wordcamp', $args);
 }
 
-function  add_acf_wordcamps( $request_data ) {
+// ACF WordCamp Custom REST API
+function  add_acf_wordcamps($request_data)
+{
   $args = array(
-      'post_type' => 'wordcamp',
-      'posts_per_page'=>-1, 
-      'numberposts'=>-1
+    'post_type' => 'wordcamp',
+    'posts_per_page' => -1,
+    'numberposts' => -1
   );
   $posts = get_posts($args);
   foreach ($posts as $key => $post) {
-      $posts[$key]->acf = get_fields($post->ID);
+    $posts[$key]->acf = get_fields($post->ID);
   }
   return  $posts;
 }
 
-function get_wordcamp_check( $request ) {
-  return current_user_can( 'edit_posts' );
+function get_wordcamp_check($request)
+{
+  return current_user_can('edit_posts');
 }
 
-add_action( 'rest_api_init', function () {
-  register_rest_route( 'acf/v1', '/wordcamp/', array(
-      'methods' => 'GET',
-      'callback' => 'add_acf_wordcamps',
-      'permission_callback' => 'get_wordcamp_check',
+add_action('rest_api_init', function () {
+  register_rest_route('acf/v1', '/wordcamp/', array(
+    'methods' => 'GET',
+    'callback' => 'add_acf_wordcamps',
+    'permission_callback' => 'get_wordcamp_check',
   ));
 });
+
+
+// Add prev / next to WP REST API
+add_filter('rest_prepare_post', function ($response, $post, $request) {
+  if ($request->get_param('per_page') === 1) {
+    global $post;
+    $next = get_adjacent_post(false, '', false);
+    $previous = get_adjacent_post(false, '', true);
+    $response->data['next'] = (is_a($next, 'WP_Post')) ? array("id" => $next->ID, "slug" => $next->post_name) : null;
+    $response->data['previous'] = (is_a($previous, 'WP_Post')) ? array("id" => $previous->ID, "slug" => $previous->post_name) : null;
+  }
+  return $response;
+}, 10, 3);
